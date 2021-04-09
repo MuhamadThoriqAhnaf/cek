22b5d3907d0ae0c43bc959ea3554ef0725ec5331
Muhamad Thoriq Ahnaf <73710647+MuhamadThoriqAhnaf@users.noreply.github.com>
2021-04-09 20:24:19 +0700

    Add files via upload

---
+++ CoffeeDrinks.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCoffeeProject
+{
+     public abstract class CoffeeDrinks
+    {
+        public string size { get; set; }
+        public string brewTime { get; set; }
+        public double price { get; set; }
+        protected const double STOREFEE = .05;
+
+        protected double retailPrice;
+
+        public double RetailPrice
+        {
+            set { calcPrice(); }
+            get { return retailPrice; }
+        }
+        public void calcPrice()
+        {
+            RetailPrice = (price * STOREFEE);
+        }
+
+    }
+
+    class HotCoffee : CoffeeDrinks
+    {
+  
+        public HotCoffee(string siz, string brewTi, double pric)
+        {
+            size = siz;
+            brewTime = brewTi;           
+            price = pric;
+        }
+    }
+    class HotLatte : CoffeeDrinks
+    {
+        public string MilkAmount { get; set; }
+
+        public HotLatte(string siz, string brewTi, double pric)
+        {
+            size = siz;
+            brewTime = brewTi;
+            price = pric;
+        }
+    }
+}
---
+++ Customer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCoffeeProject
+{
+    class Customer
+    {
+        
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        private string _favCoffee;
+
+        public string FavCoffee
+        {
+            get
+            {
+                return _favCoffee;
+            }
+
+            set
+            {
+                _favCoffee = value;
+            }
+        }
+
+        private string phoneNumber;
+        public string PhoneNumber
+        {
+            set { phoneNumber = value; }
+            get { return phoneNumber; }
+        }
+
+        private int _custId;
+
+        //set
+        public void setId(int id)
+        {
+            if (id <= 0)
+            {
+                throw new Exception("ID cant be negative number");
+            }
+            //set
+            this._custId = id;
+        }
+
+        //get
+        public int getId()
+        {
+            return this._custId;
+        }
+
+        private int _test;
+                public  const double FEE = .3;
+
+        public Customer( string F, string L,  string phone, string fCoffee)
+        {
+            Firstname = F;
+            Lastname = L;
+            _favCoffee = fCoffee;
+            PhoneNumber = phone;
+        }
+        public string DisplayCustomer()
+        {
+            return this.Firstname + " " + this.Lastname + " " + " Phone Number: " + this.phoneNumber + Environment.NewLine + "Favorite Coffee: " + this._favCoffee;
+        }
+        public string GetFullName()
+        {
+            return this.Firstname + " " + this.Lastname;
+        }
+        public double getFee()
+        {
+            return FEE;
+        }
+    }
+}
